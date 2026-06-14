11f551068fc81b72a251b1c72786ff603741c72e
delyev3852 <delyev3852@users.noreply.github.com>
2026-06-15 01:10:29 +0700

    [UFCLite] fix fighter spawn: disable DefaultPawnClass, set camera rotation

--- Source/UFCLite/FightGameMode.cpp
+++ Source/UFCLite/FightGameMode.cpp
@@ -3,10 +3,11 @@
 #include "GameFramework/PlayerStart.h"
 #include "Kismet/GameplayStatics.h"
 #include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 
 AFightGameMode::AFightGameMode()
 {
-	DefaultPawnClass = AFighterCharacter::StaticClass();
+	DefaultPawnClass = nullptr;
 }
 
 void AFightGameMode::BeginPlay()
@@ -17,6 +18,9 @@ void AFightGameMode::BeginPlay()
 
 void AFightGameMode::SpawnFighters()
 {
+	FVector SpawnPos[2];
+	FRotator SpawnRot[2];
+
 	TArray<AActor*> PlayerStarts;
 	UGameplayStatics::GetAllActorsOfClass(this, APlayerStart::StaticClass(), PlayerStarts);
 
@@ -25,38 +29,35 @@ void AFightGameMode::SpawnFighters()
 		for (int32 i = 0; i < 2; i++)
 		{
 			APlayerStart* PS = Cast<APlayerStart>(PlayerStarts[i]);
-			if (!PS) continue;
-
-			FActorSpawnParameters SpawnParams;
-			AFighterCharacter* Fighter = GetWorld()->SpawnActor<AFighterCharacter>(FighterClass, PS->GetActorLocation(), PS->GetActorRotation(), SpawnParams);
-			if (Fighter)
+			if (PS)
 			{
-				SetupFighterInput(Fighter, i);
-				if (i == 0) Fighter1 = Fighter;
-				else Fighter2 = Fighter;
+				SpawnPos[i] = PS->GetActorLocation();
+				SpawnRot[i] = PS->GetActorRotation();
 			}
 		}
 	}
 	else
 	{
-		FVector SpawnPositions[] = {
-			FVector(-200.0f, 0.0f, 100.0f),
-			FVector(200.0f, 0.0f, 100.0f)
-		};
-		FRotator SpawnRotations[] = {
-			FRotator(0.0f, 0.0f, 0.0f),
-			FRotator(0.0f, -180.0f, 0.0f)
-		};
+		SpawnPos[0] = FVector(-200.0f, 0.0f, 100.0f);
+		SpawnPos[1] = FVector(200.0f, 0.0f, 100.0f);
+		SpawnRot[0] = FRotator(0.0f, 0.0f, 0.0f);
+		SpawnRot[1] = FRotator(0.0f, -180.0f, 0.0f);
+	}
 
-		for (int32 i = 0; i < 2; i++)
+	for (int32 i = 0; i < 2; i++)
+	{
+		FActorSpawnParameters SpawnParams;
+		AFighterCharacter* Fighter = GetWorld()->SpawnActor<AFighterCharacter>(FighterClass, SpawnPos[i], SpawnRot[i], SpawnParams);
+		if (Fighter)
 		{
-			FActorSpawnParameters SpawnParams;
-			AFighterCharacter* Fighter = GetWorld()->SpawnActor<AFighterCharacter>(FighterClass, SpawnPositions[i], SpawnRotations[i], SpawnParams);
-			if (Fighter)
+			SetupFighterInput(Fighter, i);
+			if (i == 0) Fighter1 = Fighter;
+			else Fighter2 = Fighter;
+
+			APlayerController* PC = UGameplayStatics::GetPlayerController(this, i);
+			if (PC)
 			{
-				SetupFighterInput(Fighter, i);
-				if (i == 0) Fighter1 = Fighter;
-				else Fighter2 = Fighter;
+				PC->SetControlRotation(SpawnRot[i]);
 			}
 		}
 	}
